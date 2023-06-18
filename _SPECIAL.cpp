6cccd3f9c80e40d42f035f9532bb74dfdb3385c0
LeeTuah <77256247+LeeTuah@users.noreply.github.com>
2023-06-18 10:52:02 +0530

    Create _SPECIAL.cpp

---
+++ _SPECIAL.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+
+/// @brief Main function of the program
+/// @param argc Number of arguments
+/// @param argv String array of the arguments
+/// @return int
+int main(int argc, char** argv){
+    std::cout << "Word:- Special\n";
+    std::cout << "Meaning: Special defines something which is not commonly seen amongst other similar beings.\n";
+    std::cout << "Synonyms: Unique, Uncommon\n";
+    std::cout << "Antonyms: Common, Easy";
+    return 0;
+}
