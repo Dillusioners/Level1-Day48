a9422bf76aa9012003c746c478ba069110b6a45d
LeeTuah <77256247+LeeTuah@users.noreply.github.com>
2023-06-18 10:56:26 +0530

    Create Special.cpp

---
+++ Special.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+
+/// @brief Main function of the program
+/// @param argc Number of arguments
+/// @param argv String array of the arguments
+/// @return int
+int main(int argc, char** argv){
+    std::cout << "Did you know some special facts about India?\n";
+    std::cout << "In maharashtra, India, Lizards are worshipped like gods and are not killed or hunted.\n";
+    std::cout << "World\'s wettest place is located in India, called Rajasthan. World\'s driest place is also in India, called Meghalaya";
+    std::cout << "In Kota, India, ropes are used in very creative ways.\n";
+    return 0;
+}
